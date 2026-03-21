6179da6948a2fbfef5f12dc5038f04ae48e46f2e
pitchingcoachu <jgaynor@pitchingcoachu.com>
2026-03-21 13:59:53 -0700

    Change default TEAM_CODE from 'OSU' to 'SEMO'

--- scripts/rebuild_video_map_from_cloudinary.R
+++ scripts/rebuild_video_map_from_cloudinary.R
@@ -14,8 +14,8 @@ source("pitch_data_service.R")
 
 `%||%` <- function(a, b) if (is.null(a) || length(a) == 0 || all(is.na(a))) b else a
 
-team_code <- toupper(trimws(Sys.getenv("TEAM_CODE", "OSU")))
-if (!nzchar(team_code)) team_code <- "OSU"
+team_code <- toupper(trimws(Sys.getenv("TEAM_CODE", "SEMO")))
+if (!nzchar(team_code)) team_code <- "SEMO"
 
 cloud_name <- Sys.getenv("CLOUDINARY_CLOUD_NAME", "")
 api_key <- Sys.getenv("CLOUDINARY_API_KEY", "")
